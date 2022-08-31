66b56a63e87dc997784dc9f190955f978f2e618d
RafaelFreitas <raf.mec.ba@gmail.com>
2022-08-31 10:03:33 -0300

    S4-04 final

---
+++ script.js
@@ -0,0 +1,20 @@
+// function textReturn(){
+//     return "Olá mundo"
+// }
+
+// console.log(textReturn())
+
+// ---------------------------------------
+
+function textReturn(text){
+    return text
+}
+
+console.log(textReturn("Olá, estou sendo retornado pela função"))
+
+// // ---------------------------------------
+
+function textReturn(text){
+    console.log(text)
+}
+textReturn("Olá")
